5d2f61d839c6b4a1db80ca3549f613907207428a
Torumi <iljacap@gmail.com>
2023-02-11 02:40:23 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def main(a1, a2, N):
+def main(a1: int, a2: int, N: int) -> int:
     """Write your code here"""
     pass
     # return result
 
 if __name__ == '__main__':
-    a1, a2, N = tuple([int(i) for i in input().split(' ')])
-    print(main(a1, a2, N))
+    a1_, a2_, N_ = tuple([int(i) for i in input().split(' ')])
+    print(main(a1_, a2_, N_))
